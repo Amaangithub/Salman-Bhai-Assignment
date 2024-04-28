0902498bfc945c220c8ca68a3e1ed8e6ea9e632a
MD AMAAN UR RAHEMAN <adoozyone@gmail.com>
2024-04-28 18:18:41 +0530

    Fixed some issues that related iwth accordion width. Added delete box Modal

--- Salman Bhai Ass. factwise/assesment/src/Components/GetCelebs.jsx
+++ Salman Bhai Ass. factwise/assesment/src/Components/GetCelebs.jsx
@@ -1,73 +1,14 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import { Accordion } from "react-bootstrap";
-// import { BsPencil } from "react-icons/bs";
-// import { BsTrash3 } from "react-icons/bs";
-// import { MdOutlineCancel } from "react-icons/md";
-// import { TbSquareRoundedCheck } from "react-icons/tb";
-
-// const GetCelebs = () => {
-//   const [jsonData, setJsonData] = useState([]);
-
-//   const fetchData = async () => {
-//     await axios
-//       .get(`http://localhost:3000/celebrities`)
-//       .then((data) => setJsonData(data.data));
-//   };
-//   const deleteData = async (id) => {
-//     try {
-//       await axios.delete(`http://localhost:3000/celebrities/${id}`);
-//       fetchData();
-//     } catch (error) {
-//       console.error("Error deleting data:", error);
-//     }
-//   };
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       {jsonData.map((item) => {
-//         return (
-//           <div key={item.id}>
-//             <Accordion>
-//               <Accordion.Item eventKey={item.id}>
-//                 <Accordion.Header>
-//                   <div>
-//                     <img
-//                       src={item.picture}
-//                       alt="Accordion Image"
-//                       style={{
-//                         marginRight: "10px",
-//                         width: "50px",
-//                         height: "50px",
-//                       }}
-//                     />
-//                   </div>
-//                   {`${item.first} ${item.last}`}
-//                 </Accordion.Header>
-//                 <Accordion.Body>
-//                   {item.description}
-//                   <div>
-//                     <BsTrash3 color="red" onClick={() => deleteData(item.id)} />
-//                     <BsPencil color="blue" />
-//                   </div>
-//                 </Accordion.Body>
-//               </Accordion.Item>
-//             </Accordion>
-//           </div>
-//         );
-//       })}
-//       <MdOutlineCancel color="red" />
-//       <TbSquareRoundedCheck color="green" />
-//     </div>
-//   );
-// };
-
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Accordion, Col, Image, Table, Modal, Button } from "react-bootstrap";
+import {
+  Accordion,
+  Col,
+  Image,
+  Table,
+  Modal,
+  Button,
+  Card,
+} from "react-bootstrap";
 import { BsPencil, BsTrash3 } from "react-icons/bs";
 import { MdOutlineCancel } from "react-icons/md";
 import { TbSquareRoundedCheck } from "react-icons/tb";
@@ -80,6 +21,7 @@ const GetCelebs = () => {
   const handleDeleteConfirm = () => {
     if (deleteId) {
       deleteData(deleteId);
+      setShowDeleteModal(false);
     }
   };
 
@@ -113,58 +55,73 @@ const GetCelebs = () => {
     return age;
   };
 
+  // Returning the JSX
   return (
     <div>
+      {/* Mapping Data */}
       {jsonData.map((celebrity) => (
-        <div key={celebrity.id}>
-          <Accordion>
+        <div key={celebrity.id} style={{ marginBottom: "20px" }}>
+          {/* Accordion Started */}
+          <Accordion >
+            {/* Accordion for Name, Image */}
             <Accordion.Item eventKey={celebrity.id}>
-              <Accordion.Header>
-                <div>
-                  <Col>
-                    <Image
-                      src={celebrity.picture}
-                      roundedCircle
-                      style={{ width: "50px", height: "50px" }}
+              <Card style={{ width: "400px"}}>
+                <Accordion.Header>
+                  <div>
+                    <Col>
+                      <Image
+                        src={celebrity.picture}
+                        roundedCircle
+                        style={{ width: "50px", height: "50px" }}
+                      />
+                    </Col>
+                  </div>
+                  {`${celebrity.first} ${celebrity.last}`}
+                </Accordion.Header>
+
+                {/* Accordion for Age, Gender, Country & Description */}
+                <Accordion.Body>
+                  <Table>
+                    <thead style={{ color: "grey" }}>
+                      <th>Age</th>
+                      <th>Gender</th>
+                      <th>Country</th>
+                    </thead>
+                    <tbody>
+                      <td>{calculateAge(celebrity.dob)}</td>
+                      <td>{celebrity.gender.charAt(0).toUpperCase() + celebrity.gender.slice(1)}</td>
+                      <td>{celebrity.country}</td>
+                    </tbody>
+                  </Table>
+                  <Card.Text style={{ textAlign: "left" }}>
+                    {celebrity.description}
+                  </Card.Text>{" "}
+                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                    <BsTrash3
+                      color="red"
+                      onClick={() => {
+                        setShowDeleteModal(true);
+                        setDeleteId(celebrity.id);
+                      }}
                     />
-                  </Col>
-                </div>
-                {`${celebrity.first} ${celebrity.last}`}
-              </Accordion.Header>
-              <Accordion.Body>
-                <Table>
-                  <thead style={{color:"grey"}}>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Country</th>
-                  </thead>
-                  <tbody>
-                    <td>{calculateAge(celebrity.dob)}</td>
-                    <td>{celebrity.gender}</td>
-                    <td>{celebrity.country}</td>
-                  </tbody>
-                </Table>
-                {celebrity.description}
-                <div>
-                  <BsTrash3
-                    color="red"
-                    onClick={() => deleteData(celebrity.id)}
-                  />
-                  <BsPencil color="blue" />
-                </div>
-              </Accordion.Body>
+                
+                      {" "}
+                      <BsPencil color="blue" style={{ marginLeft: "30px" }}/>
+                   
+                  </div>
+                </Accordion.Body>
+              </Card>
             </Accordion.Item>
           </Accordion>
         </div>
       ))}
       <MdOutlineCancel color="red" />
       <TbSquareRoundedCheck color="green" />
+
       {/* Delete Confirmation Modal */}
       <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirm Deletion</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Are you sure you want to delete this user?</Modal.Body>
+       
+        <Modal.Header closeButton className="no-border">Are you sure you want to delete ?</Modal.Header>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>
             Cancel
